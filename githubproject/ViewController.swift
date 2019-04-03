94fdaa81f3cfa4140c515720aa1c5ab1efa689d7
John Kosewic <jkosewic2022@208-Engineering-13.local>
2019-04-03 10:46:15 -0700

    Initial Commit

---
+++ githubproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubproject
+//
+//  Created by John Kosewic on 4/3/19.
+//  Copyright © 2019 John Kosewic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
